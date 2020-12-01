8366d3ef38cb42949a9cc3942904af4d04fb4950
moloozaza456 <69285735+moloozaza456@users.noreply.github.com>
2020-12-01 11:25:44 +0700

    Add files via upload

---
+++ es6-301.js
@@ -0,0 +1,16 @@
+//The Straing
+//The basic Syntax of Strings
+// let message = "Hello World!";
+// console.log("message")
+let fname = "Mark";
+let lname = "Sakaberg";
+let fullName = fname + " " + lname;
+// console.log(fname + lname);
+console.log(fullName);
+console.log("Hello" + 12); //hello12
+console.log("10" + 2); //120
+
+console.log("10" == 10); //ture
+console.log("10" === 10); //flase
+console.log("10" !== 10); //ture
+//"10" <=> 10; // error
